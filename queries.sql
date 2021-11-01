60f2bf3654362bcd854da72c07278446513e80bb
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-01 16:48:07 -0700

    lets start, finally updated mac to 10.14

--- queries.sql
+++ queries.sql
@@ -1,10 +1,13 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers where PostalCode = '1010';
 
 -- Find the phone number for the supplier with the id 11
+SELECT Phone FROM Suppliers where SupplierID = '11';
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT * FROM Orders ORDER BY OrderDate desc LIMIT 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
 
