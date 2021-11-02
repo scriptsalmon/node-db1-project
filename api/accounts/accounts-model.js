6165898f0d615c6b7112eb45a52911a15a125d5b
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-01 20:33:37 -0700

    getById finito

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,19 +1,31 @@
 const db = require('../../data/db-config.js');
 
 // OPTION 1 - BETTER FOR TROUBLESHOOTING
-// async function getAll () { //db is an async function, so we have to await
-//   const result = db('accounts') // SELECT * FROM accounts
-// console.log(result);
+async function getAll () {
+  // const result = await db.raw('SELECT * FROM accounts;')
+  // const result = await db.select('*').from('accounts')
+  const result = await db('accounts');
+  return result;
+}
+// OPTION 2 - CLEAN
+// const getAll = () => {
+//   return db('accounts')
+// }
+
+// OPTION 1 - GET BY ID
+// async function getById (id) {
+//   // const result = await db.select('*').from('accounts').where('id', id).first()
+//   // const result = await db.select('*').from('accounts').where({ id })
+//   const result = await db('accounts').where('id', id).first()
 //   return result;
 // }
-// OPTION 2 - CLEAN
-const getAll = () => {
-  return db('accounts')
+
+// OPTION 2 - GET BY ID
+const getById = (id) => {
+  // return db('accounts').where('id', id).first();
+  return db('accounts').where({ id }).first();
 }
 
-const getById = id => {
-  return 'get wired'
-}
 
 const create = account => {
   return 'get wired'
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -5,13 +5,24 @@ router.get('/', async (req, res, next) => {
   try{
     const data = await Accounts.getAll();
     res.json(data);
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', async (req, res, next) => {
+  try {
+    const validId = await Accounts.getById(req.params.id);
+    if(validId){
+      res.json(validId);
+    } else {
+      // res.status(404).json({ message: 'id does not exist' })
+      // next();
+      next({ status: 404, message: 'account not found' })
+    }
+  } catch (err) {
+    next(err);
+  }
 })
 
 router.post('/', (req, res, next) => {
